d2e409fb21fd1fe5b802ff0c954ebd1cfa681ef4
Dennis O'Keeffe <hello@dennisokeeffe.com>
2023-06-18 20:33:55 +1000

    feat: initial landing page

---
+++ src/components/Body.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Heading } from "@chakra-ui/react";
+
+export function Body() {
+  return (
+    <Heading as="h2" size="3xl">
+      More details coming soon
+    </Heading>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Heading } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Heading as="h1" size="4xl">
+      Melbourne Japanese-English Coffee Exchange
+    </Heading>
+  );
+}
